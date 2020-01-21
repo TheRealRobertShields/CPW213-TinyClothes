53a7696692700ef77ce3898f2fadde38c1d15e3e
roberthshields93 <roberthshields93@yahoo.com>
2020-01-21 09:48:58 -0800

    Added content to home page

---
+++ TinyClothes/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Represents a single clothing item
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// The unique identifier for the clothing item
+        /// </summary>
+        [Key] //Set as PK
+        public int ItemId { get; set; }
+
+    }
+}
