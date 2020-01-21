15d15b3c0bde2a279daa3bdd5342cc5041a5e325
roberthshields93 <roberthshields93@yahoo.com>
2020-01-21 09:57:18 -0800

    Created clothing item class

---
+++ TinyClothes/Models/Clothing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Represents a single clothing item
+    /// </summary>
+    public class Clothing
+    {
+        /// <summary>
+        /// The unique identifier for the clothing item
+        /// </summary>
+        [Key] //Set as PK
+        public int ItemId { get; set; }
+
+        public string Size { get; set; }
+
+        /// <summary>
+        /// Type of clothing (shirt, pants, etc.)
+        /// </summary>
+        public string Type { get; set; }
+
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Retail price of the item
+        /// </summary>
+        public double Price { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
