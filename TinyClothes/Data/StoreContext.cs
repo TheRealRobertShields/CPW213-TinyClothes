7660848aae19a80032c69c1a93245288aa0736a4
roberthshields93 <roberthshields93@yahoo.com>
2020-01-21 10:34:39 -0800

    Added DbContext class

---
+++ TinyClothes/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Models;
+
+namespace TinyClothes.Data
+{
+    public class StoreContext : DbContext 
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        // Add a DbSet for each Entity that needs to be tracked by the DB
+        public DbSet<Clothing> Clothing { get; set; }
+    }
+}
