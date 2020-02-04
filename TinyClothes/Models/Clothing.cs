a5b1ddd802835f2dbcbc10d1d9b80854d9cebcb8
roberthshields93 <roberthshields93@yahoo.com>
2020-02-04 10:15:43 -0800

    Added data annotations for validating clothing

--- TinyClothes/Models/Clothing.cs
+++ TinyClothes/Models/Clothing.cs
@@ -17,22 +17,32 @@ namespace TinyClothes.Models
         [Key] //Set as PK
         public int ItemId { get; set; }
 
+        [Required(ErrorMessage = "Size is required")]
         public string Size { get; set; }
 
         /// <summary>
         /// Type of clothing (shirt, pants, etc.)
         /// </summary>
+        [Required]
         public string Type { get; set; }
 
+        [Required]
         public string Color { get; set; }
 
         /// <summary>
         /// Retail price of the item
         /// </summary>
+        [Required] //<-- don't need to specify required. doubles are required by default
+        [Range(0.0, 100000.0)]
         public double Price { get; set; }
 
+        [Required(ErrorMessage = "Required. No spaces or special characters")]
+        [StringLength(35)]
+        [RegularExpression("^([A-Za-z0-9])+$")]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(800)]
         public string Description { get; set; }
     }
 }
