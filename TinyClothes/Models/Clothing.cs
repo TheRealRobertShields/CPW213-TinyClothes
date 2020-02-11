0b32ad573e738818b8ee3e32c5017d812f9fc67c
Robert Shields <roberthshields93@yahoo.com>
2020-02-11 09:41:44 -0800

    Display price as currency

--- TinyClothes/Models/Clothing.cs
+++ TinyClothes/Models/Clothing.cs
@@ -30,6 +30,7 @@ namespace TinyClothes.Models
         /// Retail price of the item
         /// </summary>
         [Range(0.0, 10000.0)]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
 
         public string Title { get; set; }
