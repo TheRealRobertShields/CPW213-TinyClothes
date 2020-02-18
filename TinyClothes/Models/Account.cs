ec21a70c0c711159b941c4d8ddeea35779886717
Robert Shields <roberthshields93@yahoo.com>
2020-02-18 14:59:28 -0800

    Created a register view model

--- TinyClothes/Models/Account.cs
+++ TinyClothes/Models/Account.cs
@@ -37,4 +37,35 @@ namespace TinyClothes.Models
         public DateTime? DateOfBirth { get; set; }
 
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)] // <input type="password";
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
 }
