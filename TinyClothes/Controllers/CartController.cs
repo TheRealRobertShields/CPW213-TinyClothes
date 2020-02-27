99e51d052913b1c916bc72b7c6babeaa08552e2b
Robert Shields <roberthshields93@yahoo.com>
2020-02-27 10:47:21 -0800

    Issue#30 shopping cart (#32)
    * Added shopping cart helper
    
    * Started cart controller
    
    * started add to cart functionality (redirect user to prev url)
    
    * add to cart now redirects to exact page user was on
    
    * Added font awesome and shopping cart icon
    
    * Number of items in cart displays next to cart
    
    * User can now add multiple cart items.

---
+++ TinyClothes/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes.Controllers
+{
+    public class CartController : Controller
+    {
+        // To read cookie data.
+        private readonly StoreContext _context;
+        private readonly IHttpContextAccessor _http;
+
+        public CartController(StoreContext context, IHttpContextAccessor http)
+        {
+            _context = context;
+            _http = http;
+        }
+
+        // Display all products in cart.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Add a single product to the shopping cart.
+        public async Task<IActionResult> AddToCartAsync(int id, string prevUrl)
+        {
+            Clothing c = await ClothingDb.GetClothingById(id, _context);
+            if (c != null)
+            {
+                CartHelper.Add(c, _http);
+            }
+            return Redirect(prevUrl);
+        }
+
+        // review order page.
+        public IActionResult Checkout() 
+        {
+            return View();
+        }
+    }
+}
---
+++ TinyClothes/Models/CartHelper.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Helper class to manage user shopping cart data using cookies.
+    /// </summary>
+    public class CartHelper
+    {
+        private const string CartCookie = "CartCookie";
+
+        public static void Add(Clothing c, IHttpContextAccessor http)
+        {
+
+            List<Clothing> clothes = GetAllClothes(http);
+            clothes.Add(c);
+
+            string data = JsonConvert.SerializeObject(clothes);
+
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(14),
+                IsEssential = true,
+                Secure = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+        }
+
+        public static int GetCartCount(IHttpContextAccessor http)
+        {
+            return GetAllClothes(http).Count;
+        }
+
+        /// <summary>
+        /// Returns all clothing currently stored in the user's cookie.
+        /// If no items are present in the cookie, an empty list is returned.
+        /// </summary>
+        /// <param name="http"></param>
+        /// <returns></returns>
+        public static List<Clothing> GetAllClothes(IHttpContextAccessor http)
+        {
+            string data = http.HttpContext.Request.Cookies[CartCookie];
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return new List<Clothing>();
+            }
+            return JsonConvert.DeserializeObject<List<Clothing>>(data);
+        }
+    }
+}
